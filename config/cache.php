7486f13f533e0f7d7386f8660bbc7a90621a76fd
Richard Jacobsen <richard@kittyhawk.io>
2019-09-12 17:46:37 -0400

    make it just work

--- config/cache.php
+++ config/cache.php
@@ -35,21 +35,4 @@ return [
         ],
 
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Key Prefix
-    |--------------------------------------------------------------------------
-    |
-    | When utilizing a RAM based store such as APC or Memcached, there might
-    | be other applications utilizing the same cache. So, we'll specify a
-    | value to get prefixed to all our keys so we can avoid collisions.
-    |
-    */
-
-    'prefix' => env(
-        'CACHE_PREFIX',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_cache'
-    ),
-
 ];
